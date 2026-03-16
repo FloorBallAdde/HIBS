3311e5ed2c72b5c1d05b2576966840349274a3ba
Andreas Johansson <andreas@veyrasystems.com>
2026-03-16 13:56:46 +0100

    fix: enable button :active states on iOS Safari (touchstart trick)

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+// iOS Safari aktiverar inte :active på knappar utan detta trick
+document.addEventListener('touchstart', function(){}, { passive: true });
+
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
